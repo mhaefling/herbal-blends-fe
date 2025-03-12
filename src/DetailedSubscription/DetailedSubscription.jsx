e601a8751e401b5e7502fbd8e1b0df869e0db0b1
Matt Haefling <m.haefling@protonmail.com>
2025-03-12 12:09:07 -0600

    Attempted to finish detaile sub view, out of time, need to move on to cypress testing

--- src/DetailedSubscription/DetailedSubscription.jsx
+++ src/DetailedSubscription/DetailedSubscription.jsx
@@ -37,9 +37,12 @@ const printTeaDescription = (title) => {
 	if (tea) {
 		return (
 			<section className="teaDescription">
-				<p>Description: {tea.description}</p>
-				<p>Temperature: {tea.temp}</p>
-				<p>Brew Time: {tea.brew_time}</p>
+				<p className="subInfo">Description:</p>
+				<p className="teaValues">{tea.description}</p>
+				<p className="subInfo">Temperature:</p>
+				<p className="teaValues">{tea.temp}&deg;</p>
+				<p className="subInfo">Brew Time:</p>
+				<p className="teaValues">{tea.brew_time} - Mins</p>
 			</section>
 		)
 	} else {
@@ -47,15 +50,26 @@ const printTeaDescription = (title) => {
 	}
 };
 
-const sub_customers = subDetails?.attributes?.customers?.map((customer) => {
-	return (
-		<section>
-			<p>{customer.first_name}</p>
-			<p>{customer.last_name}</p>
-			<p>{customer.email}</p>
-			<p>{customer.sub_status}</p>
-		</section>
-	)
+const activeCustomers = subDetails?.attributes?.customers?.map((customer) => {
+	if (customer.sub_status === true) {
+		return (
+			<div>
+				<p>{customer.first_name} {customer.last_name}</p>
+				<p>{customer.email}</p>
+			</div>
+		)
+	}
+})
+
+const deactiveCustomers = subDetails?.attributes?.customers?.map((customer) => {
+	if (customer.sub_status === false) {
+		return (
+			<section>
+				<p>{customer.first_name} {customer.last_name}</p>
+				<p>{customer.email}</p>
+			</section>
+		)
+	}
 })
 
 	if (!subDetails) {
@@ -67,20 +81,58 @@ const sub_customers = subDetails?.attributes?.customers?.map((customer) => {
 	} else {
 		return (
 			<section className="HBSubDetails">
-				<h2>{subDetails.attributes.title}</h2>
-				<p>Subscription Price: {subDetails.attributes.price}</p>
-				<label for="Subscription Teas">Select a Tea:</label>
-				<select 
-					id="Teas"
-					name="HBTeas" 
-					size="8" 
-					onChange={(event) => setSelectedTeaDescription(printTeaDescription(event.target.value))}>
-
-						{sub_teas}
-				
-				</select>
-				{selectedTeaDescription}
-				{sub_customers}
+				<table>
+					<thead>
+						<tr className="subName">
+							<td colSpan="4"><h2>{subDetails.attributes.title}</h2></td>
+						</tr>
+						<tr className="SubDetails">
+							<td colSpan="2">
+								<p className="subInfo">Subscription Price:</p>
+							</td>
+							<td colSpan="2">
+								<p className="subInfo">Renews:</p>
+							</td>
+						</tr>
+						<tr className="subPricing">
+							<td colSpan="2"><p>${subDetails.attributes.price}0</p></td>
+							<td colSpan="2"><p>{subDetails.attributes.frequency}</p></td>
+						</tr>
+					</thead>
+					<tbody>
+						<tr>
+							<td colSpan="2"><p className="subInfo">Teas Included:</p></td>
+							<td colSpan="2"><p className="subInfo">Tea Description:</p></td>
+						</tr>
+						<tr>
+							<td className="subTeaDesc" colSpan="2">
+								<label className="subTeaList" for="Subscription Teas">Select Tea Description:</label>
+								<select 
+								id="Teas"
+								name="HBTeas" 
+								size="20" 
+								onChange={(event) => setSelectedTeaDescription(printTeaDescription(event.target.value))}>
+									{sub_teas}
+								</select>
+							</td>
+							<td colSpan="2" className="subTeaDesc">{selectedTeaDescription}</td>
+						</tr>
+					</tbody>
+					<tfoot>
+						<tr className="subCustmers">
+							<td colSpan="2"><p className="subInfo">Active Customers:</p></td>
+							<td colSpan="2"><p className="subInfo">Deactive Customers:</p></td>
+						</tr>
+						<tr>
+							<td className="activeCustomers">
+								{activeCustomers}
+							</td>
+							<td>
+								{deactiveCustomers}
+							</td>
+						</tr>
+					</tfoot>
+				</table>
 			</section>
 		)
 	}
